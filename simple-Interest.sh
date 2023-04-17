7031ba15e6df1a004fd9b1eb83a469434f2c3502
asritha-040 <108294537+asritha-040@users.noreply.github.com>
2023-04-17 21:47:17 +0530

    Update simple-Interest.sh

--- simple-Interest.sh
+++ simple-Interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # asritha-040
    # Input:
    # p, principal amount
    # t, time period in years
